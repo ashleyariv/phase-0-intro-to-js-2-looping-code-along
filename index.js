8e324d7767b3b6fd383041503f6c69a852bc1141
Ashley Rivera <ashleyariv@gmail.com>
2023-07-09 18:23:11 -0400

    ash looping lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(arrayOfNames) {
+    const thankYouCards = [];
+    for (let n = 0; n < arrayOfNames.length; n++) {
+        thankYouCards.push(`Thank you, ${arrayOfNames[n]}, for the wonderful surprise gift!`);
+    } 
+    return thankYouCards;
+}
+function countDown () {
+   let countDown = 10 ;
+   while (countDown > -1) {
+    console.log (countDown--);
+   }
+}
